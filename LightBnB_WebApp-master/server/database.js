3ac91d8641db91b9f8edeb16c133ec140eca9524
Graham Fujisawa <sfrrki@gmail.com>
2023-02-09 15:29:40 -0800

    adding getAllProperties to support search queries with 4 options and filter results and also be used as a way to get a user's listings

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -109,17 +109,54 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-/*
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}*/
 const getAllProperties = (options, limit = 10) => {
-  const query = 'SELECT * FROM properties LIMIT $1';
-  return pool.query(query, [limit])
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}00`);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}00`);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
+
+  // 4
+  queryString += `
+  GROUP BY properties.id
+  `;
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+  
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log('string:', queryString, 'params:', queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
     .then((result) => {
       // console.log(result.rows);
       return result.rows;
@@ -144,3 +181,14 @@ const addProperty = function(property) {
   return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
+
+/*
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE city LIKE 'Vancouver' AND cost_per_night >= 30000
+  GROUP BY properties.id
+  HAVING average_rating >= 4
+  ORDER BY cost_per_night
+  LIMIT 10;
+  */
